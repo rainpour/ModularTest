e7062022e163136c971fa055d7a82a771ba5ddff
HoyeonKim <allen.hoy@estsoft.com>
2022-06-21 15:13:41 +0900

    Add Project

---
+++ Shared/ModularTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModularTestApp.swift
+//  Shared
+//
+//  Created by 김호연 on 2022/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ModularTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
